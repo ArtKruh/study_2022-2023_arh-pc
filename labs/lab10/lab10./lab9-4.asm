677d867f24bdbb9c3752a680b3fbb943f904eb8a
Artem Kruhmalev <artkruh69@gmail.com>
2022-12-16 14:12:23 +0300

    lab10 lab11

---
+++ labs/lab10/lab10./lab9-4.asm
@@ -0,0 +1,33 @@
+%include 'in_out.asm'
+SECTION .data
+	msg1 db " Функция: f(x)=2х+15 ", 0
+	msg2 db "Результат: ", 0 
+SECTION .text 
+global _start
+_start:
+	mov eax,msg1
+	call sprintLF
+	pop ecx
+	pop edx
+	sub ecx,1
+	mov esi,0
+next:
+	cmp ecx,0h
+	jz _end
+	pop eax
+	call atoi
+	call vadim
+	add esi,eax
+	loop next
+_end: 
+	mov eax, msg2
+	call sprint
+	mov eax, esi 
+	call iprintLF
+	call quit
+
+vadim:
+	mov ebx,2
+	mul ebx
+	add eax,15
+	ret
